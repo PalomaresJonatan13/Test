bf0289e45b9a7fb11f41ada7f2b5710d39f74e37
PalomaresJonatan13 <palomaresjonatan13@gmail.com>
2025-02-19 08:25:37 -0500

    Add files via upload

---
+++ R/hello.R
@@ -0,0 +1,13 @@
+#' Fahrenheit conversion
+#'
+#' Convert degrees Fahrenheit temperatures to degrees Celsius
+#' @param F_temp The temperature in degrees Fahrenheit
+#' @return The temperature in degrees Celsius
+#' @examples
+#' temp1 <- F_to_C(50);
+#' temp2 <- F_to_C( c(50, 63, 23) );
+#' @export
+hello = function() {
+  print("hello world")
+}
+
